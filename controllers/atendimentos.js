728972e099ec997622f41a02fb15ab00a2c7791b
br3nds0n <brendson.net@gmail.com>
2021-12-17 22:19:08 -0300

    visualizacao

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -2,4 +2,9 @@ module.exports = app => {
   app.get('/atendimentos', (req, res) =>
     res.send('Voce esta na rota de atendimentos e esta realizando um GET')
   )
+
+  app.post('/atendimentos', (req, res) => {
+    console.log(req.body)
+    res.send('voce esta na de atendimentos e está realizando um post')
+  })
 }
