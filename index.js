0299455bbef9b55c443b74d20d199059c59f14d1
br3nds0n <brendson.net@gmail.com>
2021-12-17 23:17:42 -0300

    conexao database

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const customExpress = require('./config/customExpress')
+const conexao = require('./infraestrutura/conexao')
 
-const app = customExpress()
-app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+conexao.connect(erro => {
+  if (erro) {
+    console.log(erro)
+  } else {
+    const app = customExpress()
+    app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+  }
+})
